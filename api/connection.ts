5cf3f9c68cce855fc57d1fd5edbb78c9df3e3bce
Joanna <joanna.wytrzyszczewska@gmail.com>
2026-03-30 23:25:17 +0200

    fix: database variable

--- api/connection.ts
+++ api/connection.ts
@@ -1,6 +1,6 @@
 import { Pool } from 'pg';
 
-const DATABASE_URL = process.env.DATABASE_URL;
+const DATABASE_URL = process.env.DEV_DATABASE_URL;
 
 if (!DATABASE_URL) {
 	throw new Error('DATABASE_URL is not set');
